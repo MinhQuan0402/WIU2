bdc3a38f6ed30dcfb36008728dd528562ad18235
MinhQuan0402 <ngminhquan.2004@gmail.com>
2025-02-20 18:37:58 +0800

    update models

---
+++ DX1125 - CGSPRJ - WIU/DXGL-FRAMEWORK/Application/Source/SceneRingToss.h
@@ -0,0 +1,54 @@
+#ifndef SCENE_RING_TOSS_H
+#define SCENE_RING_TOSS_H
+
+#include "Scene.h"
+#include "Mesh.h"
+#include "Transform.h"
+#include "GameObject.h"
+
+class SceneRingToss : public Scene
+{
+public:
+	enum GEOMENTRY_TYPE
+	{
+		GEO_AXIS,
+		GEO_LIGHT,
+		GEO_FRONT,
+		GEO_BACK,
+		GEO_TOP,
+		GEO_BOTTOM,
+		GEO_RIGHT,
+		GEO_LEFT,
+		GEO_TEXT,
+		GEO_BOARD,
+		GEO_PLANE,
+		NUM_GEOMETRY
+	};
+
+	SceneRingToss();
+	~SceneRingToss();
+
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void LateUpdate() override;
+	virtual void Render() override;
+	virtual void Exit() override;
+
+private:
+	void RenderSkybox(void);
+	int numLight;
+	
+	enum OBJINSCENE
+	{
+		GROUND,
+		BOX, 
+		SPHERE,
+		CYLINDER,
+		TORUS,
+		NUMOBJ
+	};
+
+	GameObject* objInScene[NUMOBJ];
+};
+
+#endif
